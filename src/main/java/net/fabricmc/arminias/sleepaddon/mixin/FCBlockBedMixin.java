29612196b23cc49b6b7dbad208444edcedfa742b
Arminias <>
2022-03-02 00:14:09 +0100

    Adapted custom loom from example mod for correct refmap generation.

--- src/main/java/net/fabricmc/arminias/sleepaddon/mixin/FCBlockBedMixin.java
+++ src/main/java/net/fabricmc/arminias/sleepaddon/mixin/FCBlockBedMixin.java
@@ -10,10 +10,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FCBlockBed.class)
-public class FCBlockBedMixin extends BlockBed {
+public abstract class FCBlockBedMixin extends BlockBed {
 	public FCBlockBedMixin(int par1) {
 		super(par1);
 	}
+
 	@Inject(at = @At("HEAD"), method = "onBlockActivated", cancellable = true)
 	private void onBlockActivated(World world, int i, int j, int k, EntityPlayer player, int iFacing, float fXClick, float fYClick, float fZClick, CallbackInfoReturnable<Boolean> cir) {
 		cir.setReturnValue(super.onBlockActivated(world, i, j, k, player, iFacing, fXClick, fYClick, fZClick));
